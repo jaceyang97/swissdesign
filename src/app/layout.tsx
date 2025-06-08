72e2126caddecd4e52ca5507a7a25ae78ff7b4fc
Jace Yang <jaceyang8@gmail.com>
2025-06-08 11:33:41 +0800

    feat: add @vercel/analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "中期协 | 主页",
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="zh-CN">
       <body className="antialiased">
         {children}
+        <Analytics />
       </body>
     </html>
   );
